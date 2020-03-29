97a2829db85c8e359079a2ed9cdc63a714a4debf
Dario Leo <dario.nihil@gmail.com>
2020-03-29 12:13:24 +0200

    initial commit, created photosVC end deleted the default provided viewController

---
+++ PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Dario on 29/03/2020.
+//  Copyright © 2020 Dario. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController {
+    
+    //MARK: - Properties
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
