0daaa2992ed3362055eb32bfa6ca758704de694d
Dario Leo <dario.nihil@gmail.com>
2020-03-29 12:02:11 +0200

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Dario on 29/03/2020.
+//  Copyright © 2020 Dario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
