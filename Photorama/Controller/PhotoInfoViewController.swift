94438bd19fc1f613a0698460479b2bbb7f5261f4
Dario Leo <dario.nihil@gmail.com>
2020-04-01 17:26:01 +0200

    added a detail view controller and segue

---
+++ Photorama/Controller/PhotoInfoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Dario on 01/04/2020.
+//  Copyright © 2020 Dario. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        store.fetchImage(for: photo) { (result) in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Photorama/Controller/PhotosViewController.swift
+++ Photorama/Controller/PhotosViewController.swift
@@ -80,6 +80,20 @@ class PhotosViewController: UIViewController {
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showPhoto":
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first {
+                let photo = photos[selectedIndexPath.row]
+                let destinationVC = segue.destination as! PhotoInfoViewController
+                destinationVC.photo = photo
+                destinationVC.store = store
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+    }
+    
 //    func updateImageView(for photo: Photo) {
 //        store.fetchImage(for: photo) { (imageResult) in
 //            switch imageResult {
