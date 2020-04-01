af1a83dadf286d9bca8b622316887d1de5b2e149
Dario Leo <dario.nihil@gmail.com>
2020-04-01 16:44:24 +0200

    added a delegate for collectionView and implemented its method collectionView(willDisplay, forItemAt) to display image into cells

--- Photorama/Controller/PhotosViewController.swift
+++ Photorama/Controller/PhotosViewController.swift
@@ -59,6 +59,7 @@ class PhotosViewController: UIViewController {
         super.viewDidLoad()
         
         collectionView.dataSource = self
+        collectionView.delegate = self
         
         store = PhotoStore()
         
@@ -107,6 +108,29 @@ extension PhotosViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
-    
+}
+
+extension PhotosViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let photo = photos[indexPath.row]
+        
+        //Download the image data, which could take some time
+        store.fetchImage(for: photo) { (result) in
+            //The index path for the photo might have changed between the
+            //time the request started and finished, so find the most
+            //recent index path
+            
+            //(Note: You will have an error on the next line; you will fix it soon)
+            guard let photoIndex = self.photos.firstIndex(where: { (aPhoto) -> Bool in
+                return photo.photoID == aPhoto.photoID ? true : false
+            }), case let .success(image) = result else {
+                return
+            }
+            let photoIndexPath = IndexPath(item: photoIndex, section: 0)
+            //When the request finishes, only update the cell if it's still visible
+            if let cell = self.collectionView.cellForItem(at: photoIndexPath) as? PhotoCollectionViewCell {
+                cell.update(with: image)
+            }
+        }
+    }
 }
